3208896241024ec5b6b4909c55aa36a93633317a
Ponin Fedor <fponin@gmail.com>
2020-09-08 12:15:25 +0300

    task0304

--- 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task03.task0304;
 
 public class Solution {
     public static double addTenPercent(int i) {
-        return Double.parseDouble(null);
+        return i * 1.1; //увеличивает на 10%
 
     }
 
