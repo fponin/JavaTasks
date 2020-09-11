71cfc8a6715d8e61520a21372df4fe8839cfef8e
Ponin Fedor <fponin@gmail.com>
2020-09-11 14:53:47 +0300

    task0412

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0412;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+/* 
+Положительное и отрицательное число
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number > 0) {
+            number = number*2;
+        } else if (number < 0){
+            number = number + 1;
+        } else {
+        }
+        System.out.println(number);
+    }
+
+}
