b3eb179740ee0a2f721bd49c74e2bd9e95f84769
Ponin Fedor <fponin@gmail.com>
2020-09-15 22:31:33 +0300

    Task0505

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0505/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task05.task0505;
+
+/* 
+Кошачья бойня
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Cat pupa = new Cat("Pupa", 3, 6, 12);
+        Cat lupa = new Cat("Lupa", 5, 8, 7);
+        Cat zupa = new Cat("Zupa", 7, 4, 5);
+
+        System.out.println(pupa.fight(lupa));
+        System.out.println(lupa.fight(zupa));
+        System.out.println(zupa.fight(pupa));
+    }
+
+    public static class Cat {
+
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int ageScore = Integer.compare(this.age, anotherCat.age);
+            int weightScore = Integer.compare(this.weight, anotherCat.weight);
+            int strengthScore = Integer.compare(this.strength, anotherCat.strength);
+
+            int score = ageScore + weightScore + strengthScore;
+            return score > 0; // return score > 0 ? true : false;
+        }
+    }
+}
