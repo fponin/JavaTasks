1021acbd6f3bce94d063d62794d9dc7e497f4868
Ponin Fedor <fponin@gmail.com>
2020-09-15 22:41:16 +0300

    init game field

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,6 +1,12 @@
 package com.javarush.games.minesweeper;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Game;
 
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+
+
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
