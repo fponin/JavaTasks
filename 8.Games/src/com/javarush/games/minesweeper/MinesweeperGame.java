b25edcd6efc7ad3f0632d61ef9cff9d7dea7c727
Ponin Fedor <fponin@gmail.com>
2020-09-15 23:11:12 +0300

    setCellColor

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,12 +1,25 @@
 package com.javarush.games.minesweeper;
 
+import com.javarush.engine.cell.Color;
 import com.javarush.engine.cell.Game;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
 
 
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                gameField[i][j] = new GameObject(j,i);
+                setCellColor(i, j, Color.SILVER);
+            }
+        }
     }
 }
