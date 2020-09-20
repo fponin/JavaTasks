0b4be823dda41cef596674ef08c1c1d3b10e2d0e
fponin <fponin@gmail.com>
2020-09-20 23:13:17 +0300

    task0528

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task05.task0528;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/* 
+Вывести на экран сегодняшнюю дату
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd MM YYYY");
+
+        System.out.println(formatDate.format(date));
+    }
+}
