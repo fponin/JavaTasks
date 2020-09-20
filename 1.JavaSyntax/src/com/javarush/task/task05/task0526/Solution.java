99d4b97bd5b806607274b8f75a851679a8b13352
fponin <fponin@gmail.com>
2020-09-20 23:03:35 +0300

    task0526

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task05.task0526;
+
+/* 
+Мужчина и женщина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    Man man = new Man("Fedor", 36, "fdsgdfgsdfg");
+    Man man1 = new Man("Fedor", 36, "fdsgdfgsdfg");
+    Woman woman = new Woman("Irina", 35, "sdgfgfg");
+    Woman woman1 = new Woman("Irina", 35, "sdgfgfg");
+
+        System.out.println(man.name + " " + man.age +" "+man.address);
+        System.out.println(man1.name + " " + man1.age +" "+man1.address);
+        System.out.println(woman.name + " " + woman.age +" "+woman.address);
+        System.out.println(woman1.name + " " + woman1.age +" "+woman1.address);
+
+    }
+
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
