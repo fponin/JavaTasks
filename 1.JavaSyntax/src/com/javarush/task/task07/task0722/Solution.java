1cb9b69ec137e807b06d81b4db6a6122d00e49a1
fponin <fponin@gmail.com>
2020-10-01 23:38:54 +0300

    task0722

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0722/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task07.task0722;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Это конец
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        while (true){
+            String string = reader.readLine();
+            if (string.equals("end")){
+                break;
+            } else
+                strings.add(string);
+        }
+
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+}
