f735857dec9ef402dfbc6860392a21f66e4ee579
fponin <fponin@gmail.com>
2020-10-04 22:05:38 +0300

    task0813

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> strings = new HashSet<>();
+        strings.add("Л1");
+        strings.add("Л12");
+        strings.add("Л13");
+        strings.add("Л14");
+        strings.add("Л15");
+        strings.add("Л16");
+        strings.add("Л17");
+        strings.add("Л18");
+        strings.add("Л19");
+        strings.add("Л10");
+        strings.add("Л122");
+        strings.add("Л123");
+        strings.add("Л124");
+        strings.add("Л125");
+        strings.add("Л127");
+        strings.add("Л137");
+        strings.add("Л138");
+        strings.add("Л139");
+        strings.add("Л136");
+        strings.add("Л135");
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
