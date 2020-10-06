28cc319838cd152476fdadbef1e5effeb7989bcb
fponin <fponin@gmail.com>
2020-10-06 19:15:00 +0300

    task0820

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0820/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task08.task0820;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Множество всех животных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<>();
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        Set<Dog> dogs = new HashSet<>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        return dogs;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        Set<Object> pets = new HashSet<>();
+        pets.addAll(cats);
+        pets.addAll(dogs);
+        return pets;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        for (Cat cat : cats) {
+            pets.remove(cat);
+        }
+    }
+
+    public static void printPets(Set<Object> pets) {
+        for (Object pet : pets) {
+            System.out.println(pet);
+        }
+    }
+
+    public static class Cat {
+    }
+
+    public static class Dog {
+    }
+
+}
