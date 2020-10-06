21976511c66ef40c5fb8ff6b1dbf2beff933de53
fponin <fponin@gmail.com>
2020-10-06 22:29:12 +0300

    task0822

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        int min = array.get(0);
+        for (Integer i : array) {
+            if (min > i){
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            arrayList.add(Integer.parseInt(reader.readLine()));
+        }
+        return arrayList;
+    }
+}
